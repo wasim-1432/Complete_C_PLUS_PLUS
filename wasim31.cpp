460d4190ef0345039beb137603c2aad761d299fb
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-07-06 16:21:47 +0530

    Write a overloaded function calculate volume of cuboid,cone and sphare.

---
+++ wasim31.cpp
@@ -0,0 +1,12 @@
+int Calculate_Volume_Using_Function_Overloading(int length,int breadth,int height)
+{
+    return length*breadth*height;
+}
+float Calculate_Volume_Using_Function_Overloading(int r,int h)
+{
+    return 3.14*r*r*h/3;
+}
+float Calculate_Volume_Using_Function_Overloading(int r)
+{
+    return 4*3.14*r*r*r/3;
+}
